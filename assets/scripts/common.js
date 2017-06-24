9fd53078ce2aaf1b818d245c183f6c777e024925
ecmascriptguru <ecmascript.guru@gmail.com>
2017-06-24 05:24:28 -0400

    common.js file refined

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -15,82 +15,108 @@ let restAPI = (function(window, jQuery) {
 
 	_v1ApiBaseUrl = _mainHost + "api/v1/";
 	let _settings = {
-			"url": _v1ApiBaseUrl + "iSamples",
-			"method": "POST",
-			"data": {
-				"token": JSON.parse(localStorage._token || "null"),
-			},
-			"headers": {
-				"accept": "application/json"
-			}
-		};
+		"url": _v1ApiBaseUrl + "iSamples",
+		"method": "POST",
+		"data": {  },
+		"headers": {
+			"accept": "application/json"
+		}
+	};
 
 	/**
-	 * 
+	 * Send request to API to the specific url with given params.
+	 * @param {string} url 
+	 * @param {object} params 
+	 * @param {function} success 
+	 * @param {function} failure 
+	 */
+	const sendRequest = (url, params, success, failure) => {
+		_settings.url = url;
+		_settings.data = params;
+		$.ajax(_settings).done((response) => {
+			if (response.status) {
+				if (typeof success === "function") {
+					success(response);
+				}
+			} else {
+				if (typeof failure === "function") {
+					failure(response);
+				}
+			}
+		});
+	}
+
+	/**
+	 * Getting initial sample parameters to compute estimation of unit sales.
 	 * @param {string} domain 
 	 * @param {string} category 
 	 * @param {function} success 
 	 * @param {function} failure 
 	 */
 	const getInitialSamples = (domain, category, success, failure) => {
-			_settings.data.domain = domain;
-			_settings.data.category = category;
-			_settings.data.token = JSON.parse(localStorage._token);
-			$.ajax(_settings).done((response) => {
-				if (response.status) {
-					if (typeof success === "function") {
-						success(response.samples);
-					}
-				} else {
-					if (typeof failure === "function") {
-						failure();
-					}
-				}
-			});
+		let params = {
+			domain: domain,
+			category: category,
+			token: JSON.parse(localStorage._token),
+		}
+
+		sendRequest(_v1ApiBaseUrl + "iSamples", params, success, failure);
+	};
+
+	/**
+	 * Getting all of products being tracked by authenticated user for given domain and category.
+	 * @param {string} domain 
+	 * @param {string} category 
+	 * @param {function} success 
+	 * @param {function} failure 
+	 */
+	const getTrackingProducts = (domain, category, success, failure) => {
+		let params = {
+			domain: domain,
+			category: category,
+			token: JSON.parse(localStorage._token),
+		}
+		sendRequest(_v1ApiBaseUrl + "items", params, success, failure);
+	}
+
+	/**
+	 * Register user with name, email address and password.
+	 * @param {string} name 
+	 * @param {string} email 
+	 * @param {string} password 
+	 * @param {function} callback 
+	 * @param {function} failure
+	 */
+	const register = function(name, email, password, success, failure) {
+		let params = {
+			name: name,
+			email: email,
+			password: password
 		};
 
-	const register = function(name, email, password, callback) {
-			$.ajax({
-				url: _v1ApiBaseUrl + "register",
-				data: JSON.stringify({
-					name: name,
-					email: email,
-					password: password
-				}),
-				method: "post",
-				contentType: "application/json",
-				success: function(res) {
-					if (typeof callback == "function") {
-						callback(res);
-					} else {
-						console.log(res);
-					}
-				}
-			});
-		};
+		sendRequest(_v1ApiBaseUrl + "register", params, success, failure);
+	};
 
-	const login = function(email, password, callback) {
-			$.ajax({
-				url: _v1ApiBaseUrl + "login",
-				data: JSON.stringify({
-					email: email,
-					password: password
-				}),
-				method: "post",
-				contentType: "application/json",
-				success: function(res) {
-					if (typeof callback == "function") {
-						callback(res);
-					} else {
-						console.log(res);
-					}
-				}
-			});
-		};
+	/**
+	 * Login with email address and password.
+	 * @param {string} email 
+	 * @param {string} password 
+	 * @param {function} success 
+	 * @param {function} failure
+	 */
+	const login = function(email, password, success, failure) {
+		let params = {
+			email: email,
+			password: password
+		}
+
+		sendRequest(_v1ApiBaseUrl + "login", params, success, failure);
+	};
 
 	return {
 		base: _mainHost,
 		apiBaseUrl: _v1ApiBaseUrl,
+		trackings: getTrackingProducts,
 		samples: getInitialSamples,
 		register: register,
 		login: login
